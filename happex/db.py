5c2bbe4ba696289c0a5737b335c14499f79f5db5
Andrew <andrewlreilly@gmail.com>
2019-12-01 19:06:06 -0500

    Some updates to db.py and main.py beginning process of designing queries

--- happex/db.py
+++ happex/db.py
@@ -33,9 +33,65 @@ class DB:
         cur.close()
         return rows
 
-    def get_scores(self):
+
+
+# Functions to use for initial map:
+    # List of countries in order by country
+    def get_2018_countries(self):
         cur = self.db.cursor()
-        cur.execute("SELECT country, score FROM score ORDER BY country;")
-        scores = cur.fetchall()
+        country = []
+        cur.execute("SELECT country FROM happy WHERE year=2018 ORDER BY country;")
+        c = cur.fetchall()
         cur.close()
-        return scores
+        for item in c:
+            country.append (item[0])
+        return country
+    
+    # List of life ladder scores in order by country
+    def get_2018_ladder(self):
+        cur = self.db.cursor()
+        ladder = []
+        cur.execute("SELECT lifeladder FROM happy WHERE year=2018 ORDER BY country;")
+        c = cur.fetchall()
+        cur.close()
+        for item in c:
+            ladder.append (item[0])
+        return ladder
+    
+    # List of positive affects in order by country
+    def get_2018_paffect(self):
+        cur = self.db.cursor()
+        paffect = []
+        cur.execute("SELECT paffect FROM happy WHERE year=2018 ORDER BY country;")
+        c = cur.fetchall()
+        cur.close()
+        for item in c:
+            paffect.append (item[0])
+        return paffect
+    
+    # List of negative affects in order by country
+    def get_2018_naffect(self):
+        cur = self.db.cursor()
+        naffect = []
+        cur.execute("SELECT naffect FROM happy WHERE year=2018 ORDER BY country;")
+        c = cur.fetchall()
+        cur.close()
+        for item in c:
+            naffect.append (item[0])
+        return naffect
+
+    # Using Score table
+    # List of countries and scores 
+    def get_world_scores(self):
+        cur = self.db.cursor()
+        country = []
+        score = []
+        cur.execute("SELECT country, score FROM score ORDER BY country;")
+        c = cur.fetchall()
+        cur.close()
+        for item in c:
+            country.append (item[0])
+            score.append (item[1])
+        return country, score
+    
+    
--- happex/main.py
+++ happex/main.py
@@ -1,21 +1,47 @@
 from happex import app
 
-
+#Homepage - worldmap data or options for each page?
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #initial map:
+    country, score = app.db.get_world_scores()
+    
+    #plotting
+    plot = worldmap_plot
+    return plot
 
+def query():
+    #some way to display and import map options
+    select = query()
+    
+    #loading in data
+    countries = app.db.get_2018_countries
+    ladder = app.db.get_2018_ladder
+    paffect = app.db.get_2018_paffect
+    naffect = app.db.get_2018_naffect
+    
+    #if statements to choose what to plot by
+    if select == "ladder":
+        ydata = ladder
+    if select == "paffect":
+        ydata = paffect
+    if select == "naffect":
+        ydata = naffect
+    
+    #replotting worldmap with selected data
+    plot = plt.plot(countries, ydata)
 
+#Scores Page
 @app.route("/scores")
 def scores():
     return app.db.get_scores()
 
-
+#Generosity
 @app.route("/generosity")
 def generosity():
     return app.db.get_year(year=2018, cols=("country", "generosity", "paffect"))
 
-
+#Country
 @app.route("/country/<country>")
 def country():
     return app.db.get(country_name=country, cols=("year", "paffect"))
